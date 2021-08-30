16e67cc7994fe86d97554ef4fed028dff92b9e3d
FediAbdelkebir <abdelkebir.fedi@esprit.tn>
2021-08-30 21:54:52 +0100

    Heroku Case Sensetive

--- src/Server/server.js
+++ src/Server/server.js
@@ -20,6 +20,11 @@ app.use(express.json());
 app.use(cors());
 
 //routes
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 
 app.use("/users", userRoutes);
 app.use("/taches", tacheRoutes);
